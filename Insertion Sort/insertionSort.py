6d491cb822ed224e837e8d7d83962493ea1ebe8c
Rathin <rathink4@gmail.com>
2023-01-23 13:24:28 -0600

    Added insertion sort

---
+++ Insertion Sort/insertionSort.py
@@ -0,0 +1,21 @@
+def insertion_sort(list):
+    # Since the 0th index in the list will be sorted by default (single number is sorted inherently), 
+    # you start from the 1st index of the list
+    for index in range(1, len(list)):
+        value = list[index]
+        j = index - 1
+
+        # Now, you compare each value to the previous one and check if it is less than it, 
+        # If it is then you shift the values to place the lower value in it's spot
+        while(j >= 0 and value < list[j]):
+            list[j+1] = list[j]
+            j -= 1
+        
+        # If the value is not lower, then you found where the value should be placed. Place it on that index
+        list[j+1] = value
+    
+    return list
+
+
+example = [3, 6, 5, 8, 2, 1]
+print(insertion_sort(example))
